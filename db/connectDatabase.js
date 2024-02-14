ebc5c182a71bfd5aa722935d0857b48a5d565922
freddieswift <freddie9117@live.co.uk>
2024-02-14 19:28:27 +0000

    set database name based on node_env

--- db/connectDatabase.js
+++ db/connectDatabase.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-const database = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+let databaseURL = process.env.DATABASE_URL.replace('<password>', process.env.DATABASE_PASSWORD)
 
-module.exports = mongoose.connect(database, {
+if (process.env.NODE_ENV === 'development') {
+    databaseURL = databaseURL.replace('<database_name>', process.env.DATABASE_TEST)
+}
+else if (process.env.NODE_ENV === 'production') {
+    databaseURL = databaseURL.replace('<database_name>', process.env.DATABASE_PROD)
+}
+
+module.exports = mongoose.connect(databaseURL, {
     useNewUrlParser: true
 }).then((connection) => {
     console.log("DB connection successful")
