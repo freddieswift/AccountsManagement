8d865008a19ffa9ec1b2318b22a135313080bcc8
freddieswift <freddie9117@live.co.uk>
2024-02-26 22:22:38 +0000

    check if password has been modified before hashing

--- models/userModel.js
+++ models/userModel.js
@@ -34,13 +34,12 @@ const userSchema = new mongoose.Schema({
 
 //hash password
 userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.checkPassword = async function (passwordToCompare) {
-    console.log(passwordToCompare)
-    console.log(this)
     if (!await bcrypt.compare(passwordToCompare, this.password)) {
         throw new CustomError('invalid credentials', 400)
     }
